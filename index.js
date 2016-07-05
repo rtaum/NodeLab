73f04d5ca1f564026713c01d5a722e87a6e021d8
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2016-07-05 23:31:06 +0200

    simple jade layout is implemented.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var express = require('express');
-var path = require('path');
 var home = require('./routes/home.js');
-var customers = require('./routes/customer.js');
+
+var pub = __dirname + '/public';
 
 var app = express();
-app.set('view engine', 'jade');
+app.use(express.static(pub));
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-var port = 5000;
+app.set('view engine', 'jade');
 
 app.get('/', home.index);
-app.get('/customers/:id', customers.index);
-app.get('/customers/contact', customers.contact);
 
-app.listen(port);
+app.listen(3000);
--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,3 @@
 exports.index = function (req, res) {
-  res.send('Welcome to the home section');
+  res.render('home');
 }
